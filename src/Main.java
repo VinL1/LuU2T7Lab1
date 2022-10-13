48973b8f737b9214b84bdb9d33ffd706c96e30fd
Alexandre Arturi <alexandrea8@nycstudents.net>
2022-10-13 09:27:28 -0400

    alkdsjfl

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main (String [] args) {
 
@@ -16,5 +19,38 @@ public class Main {
         String x2 = today.substring(3, 7).substring(0, 3);
         System.out.println(x2);
 
+        String blah2 = "What's for dinner?";
+        String agh2 = blah2.substring (11);
+
+        String agh3 = blah2.substring (11,18);
+
+        Scanner input = new Scanner (System.in);
+
+        System.out.println ("Enter first string: ");
+        String first = input.nextLine ();
+
+        System.out.println ("String length: " + first.length ());
+        System.out.println ("First half: " + first.substring (0, (first.length() / 2)));
+        System.out.println ("Second half: " + first.substring ((first.length() / 2), first.length()));
+
+        System.out.println ("Enter second string: ");
+        String second = input.nextLine ();
+
+        if (first.length () == second.length ()) {
+            System.out.println ("Both strings have the same length");
+        }
+        else if (first.length () > second.length ()) {
+            System.out.println (first + " is longer");
+        }
+        else {
+            System.out.println (second + " is longer");
+        }
+
+       if (first.compareTo (second) == 0) {
+           System.out.println ("Both strings have the same length");
+       }
+        else if (first.compareTo (second) > 0) {
+            System.out.println ("")
+        }
     }
 }
